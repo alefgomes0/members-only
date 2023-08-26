cab01f1664413e545a32e1fbe92475bca3a156b6
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-08-25 21:02:51 -0300

    Begin to implement routes/controllers

---
+++ controllers/messageBoardController.js
@@ -0,0 +1,5 @@
+const User = require("../models/user");
+const Message = require("../models/message");
+const { body, validationResult } = require("express-validator");
+const asyncHandler = require("express-async-handler");
+
