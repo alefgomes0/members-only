aecd252cecbc7810ddb682eff0995e498cf3714f
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-08-27 16:32:55 -0300

    Begin to create register post route

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -4,8 +4,16 @@ const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 
 
-exports.get = asyncHandler((req, res, next) => {
+exports.get = (req, res, next) => {
   res.render("register", {
     title: "Create an account"
   })
-})
+}
+
+exports.post = [
+  body("first_name")
+    .trim()
+    .escape()
+    .withMessage("First name must not be empty")
+    .isLength({ min: 2, max: 40 })
+]
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const messageBoard = require("../controllers/messageBoardController");
-const register = require("../controllers/register")
-
+const register = require("../controllers/registerController");
 
 router.get("/", messageBoard.index);
-router.get("/register", register.get)
+router.get("/register", register.get);
 
-
-module.exports = router
+module.exports = router;
