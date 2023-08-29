b7a1c6e3178f97598bbfa48c2c429251e2b7115a
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-08-28 23:43:17 -0300

    Begin to implement new message form

--- controllers/messageBoardController.js
+++ controllers/messageBoardController.js
@@ -5,9 +5,17 @@ const asyncHandler = require("express-async-handler");
 
 exports.index = asyncHandler(async (req, res, next) => {
   const allMessages = await Message.find().populate("user").exec();
+  let showMessageForm = true;
+
+  function setShowForm() {
+    showMessageForm = !showMessageForm
+  }
+
 
   res.render("index", {
     title: "Members Only",
     messages: allMessages,
+    showMessageForm: showMessageForm,
+    setShowForm: setShowForm
   });
 });
