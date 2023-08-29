b2a84c1b013a4f037452f836820902a139b5fc74
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-08-29 14:34:53 -0300

    Change member routes, bug fixes

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -9,31 +9,33 @@ exports.get = (req, res, next) => {
 };
 
 exports.post = [
-  body("question")
-    .trim()
-    .escape()
-    .toLowerCase(),
-  
+  body("question").trim().escape().toLowerCase(),
+
   asyncHandler(async (req, res, next) => {
     const errors = validationResult(req);
-    const rightAnswer = req.body.question === "cat" || req.body.question === "cats" ? true : false;
-    const user = await User.findByIdAndUpdate(req.user._id, { is_member: true })
+    const rightAnswer =
+      req.body.question === "cat" || req.body.question === "cats"
+        ? true
+        : false;
+
+    if (req.user.is_member) {
+      res.render("member", {
+        title: "Become a member",
+        error: "You're already a member!",
+      });
+    }
 
     if (!rightAnswer) {
       res.render("member", {
         title: "Become a member",
-        error: "Wrong answer"
-      })
-    } else if (req.user.is_member) {
-      res.render("member", {
-        title: "Become a member",
-        error: "You're already a member!"
-      })
+        error: "Wrong answer",
+      });
     } else {
+      const user = await User.findByIdAndUpdate(req.user._id, {
+        is_member: true,
+      });
       await user.save();
-      res.redirect("/")
+      res.redirect("/");
     }
-
-
-  })
-]
+  }),
+];
