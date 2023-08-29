625d7d22e98b88339cf7b27130f08e4ce01a344c
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-08-28 21:56:43 -0300

    Add new message button

--- app.js
+++ app.js
@@ -36,8 +36,8 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 app.use((req, res, next) => {
-  res.locals.currentUser = req.user;
-  next();
+  res.locals.currentUser = req.user
+  next()
 });
 
 app.use("/", indexRouter);
--- config/passport.js
+++ config/passport.js
@@ -5,20 +5,18 @@ const User = require("../models/user");
 
 passport.use(
   new LocalStrategy(async (username, password, done) => {
-    console.log("aaaaaaaaaaaaaa");
     try {
       const user = await User.findOne({ email: username });
 
       if (!user) {
-        console.log("111111111111111")
         return done(null, false, { message: "Incorrect username" });
       }
 
       const match = await bcrypt.compare(password, user.password);
       if (!match) {
-        console.log("2222222222")
         return done(null, false, { message: "Incorrect password " });
       }
+      
       return done(null, user);
     } catch (err) {
       return done(err);
